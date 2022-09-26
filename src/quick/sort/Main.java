bfa92c27ac35120150aef887bec6d548773bf847
evnag <vskonev22@gmail.com>
2022-09-26 18:13:37 +0300

    complete quick sort

--- src/quick/sort/Main.java
+++ src/quick/sort/Main.java
@@ -2,6 +2,20 @@ package quick.sort;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        char[] a = {'d', 'x', 'a', 'r', 'p', 'j', 'i'};
+        int i;
+
+        System.out.println("Исходный массив: ");
+        for (i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+        System.out.println();
+        QuickSort.qsort(a);
+        System.out.println("Отсортирований массив: ");
+        for (i = 0; i < a.length; i++) {
+            System.out.print(a[i] + "\t");
+        }
     }
 }
